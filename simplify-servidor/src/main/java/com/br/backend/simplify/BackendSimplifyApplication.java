562020e8a0ef9ee0ae44a219a235ad161b19755d
Boris <boris@email.com>
2023-09-30 03:46:53 -0300

    backend-simplify | modulos criados

---
+++ simplify-servidor/src/main/java/com/br/backend/simplify/BackendSimplifyApplication.java
@@ -0,0 +1,13 @@
+package com.br.backend.simplify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSimplifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSimplifyApplication.class, args);
+	}
+
+}
